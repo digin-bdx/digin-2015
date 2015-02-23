005eb4f5238e71a8111260e9e58356b76c5aba21
bltnico <bellot.n@gmail.com>
2015-02-23 15:02:45 +0100

    Add CNRS

--- app/scripts/controllers/comingsoon.js
+++ app/scripts/controllers/comingsoon.js
@@ -6,14 +6,16 @@
  *
  * @param {Object} $scope
  * @param {Object} $http
+ * @param {Object} $log
  */
 angular.module('digin')
-    .controller('ComingSoonCtrl', function ($scope, $http) {
+    .controller('ComingSoonCtrl', function ($scope, $http, $log) {
 
         $scope.consummers = {};
 
         $http.get('dummy/consummers.json').success(function(data){
             $scope.consummers = data.consummers;
+            $log.info($scope.consummers);
         });
 
     });
